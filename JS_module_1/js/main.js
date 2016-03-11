fdb5ed0b62fd6133db3fa96dbbeba78ff36a300b
Strelcov Ivan <ivan.strelcov.1986@gmail.com>
2016-03-11 23:15:26 +0200

    added 1 module for js

---
+++ JS_module_1/js/main.js
@@ -0,0 +1,32 @@
+function pow(){
+    var x = prompt('Введите число');
+  var n = prompt('Введите степень');
+  var result = x;
+  for (var i = 1; i < n; i++) {
+    result *= x;
+  }
+  return result;
+}
+alert(pow());
+function login(){
+	var names = [];
+	for (var i = 1; i < 6; i++) {
+		var name = prompt('Введите имя ' + i);
+		names.push(name);
+	}
+	var log = prompt('Введите ваше имя');
+	for (var i = 0; i < names.length; i++){
+		if ( log == names[i] ) {
+			alert('Hello ' + names[i]);
+			return;
+		}
+		else if ( log != names[i] && i != names.length-1){
+	   		continue;
+		}
+		else {
+			alert('NOname');
+			return;
+		}
+	} 
+}
+login();
