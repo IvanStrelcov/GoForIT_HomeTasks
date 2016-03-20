84592bfa3c0d2116ac252dcc618511be4f6f5ea1
Strelcov Ivan <ivan.strelcov.1986@gmail.com>
2016-03-20 16:22:51 +0200

    fixed your comments

--- JS_module_2/js/main.js
+++ JS_module_2/js/main.js
@@ -1,73 +1,149 @@
 var data = {
 	'pageTitle': 'Тест по программированию',
-	'questionTitle': '. Вопрос №',
-	'label': 'Вариант ответа №'
-}
+	'questions': [
+		{
+			'questionTitle': '1. Вопрос №1',
+			'questionList': [
+				{
+					'label': 'Вариант ответа №1',
+					'id': 'checkbox-1',
+					'type': 'checkbox',
+					'for': 'checkbox-1'
+				},
+				{
+					'label': 'Вариант ответа №2',
+					'id': 'checkbox-2',
+					'type': 'checkbox',
+					'for': 'checkbox-2'
+				},
+				{
+					'label': 'Вариант ответа №3',
+					'id': 'checkbox-3',
+					'type': 'checkbox',
+					'for': 'checkbox-3'
+				}]
+
+		},
+		{
+			'questionTitle': '2. Вопрос №2',
+			'questionList': [
+				{
+					'label': 'Вариант ответа №1',
+					'id': 'checkbox-4',
+					'type': 'checkbox',
+					'for': 'checkbox-4'
+				},
+				{
+					'label': 'Вариант ответа №2',
+					'id': 'checkbox-5',
+					'type': 'checkbox',
+					'for': 'checkbox-5'
+				},
+				{
+					'label': 'Вариант ответа №3',
+					'id': 'checkbox-6',
+					'type': 'checkbox',
+					'for': 'checkbox-6'
+				}]
+		},
+		{
+			'questionTitle': '3. Вопрос №3',
+			'questionList': [
+				{
+					'label': 'Вариант ответа №1',
+					'id': 'checkbox-7',
+					'type': 'checkbox',
+					'for': 'checkbox-7'
+				},
+				{
+					'label': 'Вариант ответа №2',
+					'id': 'checkbox-8',
+					'type': 'checkbox',
+					'for': 'checkbox-8'
+				},
+				{
+					'label': 'Вариант ответа №3',
+					'id': 'checkbox-9',
+					'type': 'checkbox',
+					'for': 'checkbox-9'
+				}]
+		}
+	]
+};
 var dom = {
-	key1: function addHeader() {
+	addWrapper: function() {
+		var div = document.createElement('div');
+		div.className = 'wrapper';
+		document.body.appendChild(div);
+	},
+	addHeader: function() {
 		var div = document.createElement('div');
 		div.className = 'header';
-		document.body.appendChild(div);
+		document.querySelector('.wrapper').appendChild(div);
 
 		var titlePage = document.createElement('h1');	
 		titlePage.innerHTML = data.pageTitle;		
 		titlePage.className = 'header-title';
 		div.appendChild(titlePage);
 	},
-	key2: function addMain() {
-		var id = 1;
+	addMain: function() {
 		var main = document.createElement('div');
 		main.className = 'main';
-		document.body.appendChild(main);
+		document.querySelector('.wrapper').appendChild(main);
+	},
+	addForm: function() {
+		var form = document.createElement('form');
+		form.className = 'form';
+		form.setAttribute ('action','#');
+		document.querySelector('.main').appendChild(form);
 
-			for (var i = 1; i < 4; i++) {
+		for (var i = 0; i < data.questions.length; i++) {
 				var questionTitle = document.createElement('p');
-				questionTitle.className = 'ul-title ul-title-' + i;
-				questionTitle.innerHTML = i + data.questionTitle + i;
-				main.appendChild(questionTitle);
+				questionTitle.className = 'ul-title ul-title-' + (i+1);
+				questionTitle.innerHTML = data.questions[i].questionTitle;
+				form.appendChild(questionTitle);
 
 				var list = document.createElement('ul');
 				list.className = 'list list-' + i;
-				main.appendChild(list);
+				form.appendChild(list);
 				
-				for (var j = 1; j < 4; j++) {
+				for (var j = 0; j < data.questions[i].questionList.length; j++) {
 					var item = document.createElement('li');
-					item.className = 'item item-'+ j;
+					item.className = 'item item-'+ (j+1);
 					list.appendChild(item);
 
 					var checkBox = document.createElement('input');
 					checkBox.className = 'my-checkbox';
-					checkBox.setAttribute('type', 'checkbox');
-					checkBox.setAttribute('id', 'checkbox' + id);					
+					checkBox.setAttribute('type', data.questions[i].questionList[j].type);
+					checkBox.setAttribute('id', data.questions[i].questionList[j].id);					
 					item.appendChild(checkBox);
 
 					var label = document.createElement('label');
 					label.className = 'my-label';
-					label.innerHTML = data.label + j;
-					label.setAttribute('for', 'checkbox' + id);
+					label.innerHTML = data.questions[i].questionList[j].label;
+					label.setAttribute('for', data.questions[i].questionList[j].id);
 					item.appendChild(label);
-
-					id ++;
 				}
-				
-			}
+		}
 	},
-	key3: function addBtn() {
-		var footer = document.createElement('div');
-		footer.className = 'footer';
-		document.body.appendChild(footer);
+	addBtn: function() {
+		var div = document.createElement('div');
+		div.className = 'btn-box';
+		document.querySelector('.form').appendChild(div);
 
 		var button = document.createElement('input');
-		button.className = 'button';
+		button.className = 'btn';
 		button.setAttribute('type', 'submit');
 		button.setAttribute('value', 'Проверить мои результаты');
-		footer.appendChild(button);
+		div.appendChild(button);
 
 	},
-	key: function assembly() {
-		dom.key1();
-		dom.key2();
-		dom.key3();
+	assembly: function() {
+		dom.addWrapper();
+		dom.addHeader();
+		dom.addMain();
+		dom.addForm();
+		dom.addBtn();
 	},
 }
-dom.key();
+dom.assembly();
